88ecb4100edd1fd8323f91a53f47c65b65951ac5
chychey <greenchy222@gmail.com>
2023-12-06 11:55:56 -0800

    commit

--- Project5_comparisons_type_coercion/main.js
+++ Project5_comparisons_type_coercion/main.js
@@ -1,4 +1,54 @@
-document.write(typeof "cat")
-function my_Function() {
+document.write(typeof "cat");
+function my_Function1() {
     document.getElementById("Test").innerHTML = 0/0;
+}
+function my_Function2() {
+    document.getElementById("Test1").innerHTML = isNaN('cats');
+}
+function my_Function3() {
+    document.getElementById("Test2").innerHTML = isNaN('18');
+}
+document.write(4E412);
+
+document.write(-3E12);
+
+document.write(12 > 2);
+
+document.write(12 < 2);
+
+console.log(6 + 6);
+
+document.write(12 == 12);
+
+document.write(4 == 12);
+
+S = 15;
+N = 15;
+document.write(S === N);
+
+B = 8;
+G = 9;
+document.write(B === G);
+
+A = "cat";
+B = "cat";
+document.write(A === B);
+
+K = 9;
+M = "9";
+document.write(K === M);
+
+document.write(8 > 2 && 14 > 8);
+
+document.write(8 < 2 && 14 < 8);
+
+document.write(9 > 10 || 9 > 3);
+
+document.write(9 > 20 || 15 > 20);
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(25 > 5);
+}
+function not_Function2() {
+    document.getElementById("Not2").innerHTML = !(5 > 10);
 }
