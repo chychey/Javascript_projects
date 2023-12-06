76cce63a0afddc8700f14a6d450e10e8454bbe47
chychey <greenchy222@gmail.com>
2023-12-05 18:05:09 -0800

    commit

---
+++ Project1_expressions_alert/basicchallenge.js
@@ -0,0 +1,6 @@
+var B = "6" + "6"
+document.write(B);
+let x = 5; //assigning the value to x
+let y = 6; //assigning the value to y
+let z = x + y;
+document.getElementById("demo") .innerHTML = "The sum of x + y is:" + z;
