54908f2a455cd37b6e13a5cea3405b4b2b1df5f6
chychey <greenchy222@gmail.com>
2023-12-27 14:43:46 -0800

    push

--- Project6_ternary__operators_construtors/JS/main.js
+++ Project6_ternary__operators_construtors/JS/main.js
@@ -31,7 +31,7 @@ function myFunction(){
  function count_Function() {
     document.getElementById("Counting").innerHTML = Count();
     function Count() {
-        var starting_point = 9;
+        var Starting_point = 9;
         function Plus_one() {Starting_point += 1;}
         Plus_one();
         return Starting_point;
