565fcd6e407bbb3775d849230eaa3cc170908894
chychey <greenchy222@gmail.com>
2023-12-28 14:18:34 -0800

    commit

---
+++ Project8_string_methods/main.js
@@ -0,0 +1,48 @@
+function slice_Method() {
+    var Sentence = "All work and no play makes chy a dull girl.";
+    var Section = Sentence.slice(27,33);
+    document.getElementById("Slice").innerHTML = Section; 
+}
+
+function string_Method() {
+    var X =182;
+    document.getElementById("Numbers_to_string").innerHTML = X.toString();
+}
+
+function precision_Method() {
+    var X = 12355499559.1243333;
+    document.getElementById("Precision").innerHTML = X.toPrecision(10);
+}
+
+function uppercase_Method() {
+    let text = "Hey Dawg";
+    let result = text.toUpperCase();
+
+    document.getElementById("demo").innerHTML = result;
+}
+
+function text_search() {
+    let tex = "ms. red has a Red house"
+    let position = text.search("red");
+    document.getElementById("black").innerHTML = position;
+}
+
+function fixed_Method() {
+    let num = 5.10000;
+            let n = num.toFixed();
+            document.getElementById("promo").innerHTML = n;
+}
+
+function value_Method() {
+    let  = "Hello World!";
+            let result = text.valueOf();
+
+            document.getElementById("can").innerHTML = result;
+}
+
+function text_connection() {
+    let text1 = "Dog";
+    let text2 = "house";
+    let result = text1.concat(" ",text2);
+    document.getElementById("connection").innerHTML = result;
+}
