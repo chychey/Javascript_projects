e46236868bf75aa87004d99c5d83b183dd3ddd1c
chychey <greenchy222@gmail.com>
2024-01-08 16:39:28 -0800

    commit

--- JavaScript Projects/TicTacToe/js/tictactoe.js
+++ JavaScript Projects/TicTacToe/js/tictactoe.js
@@ -14,7 +14,7 @@ function placeXOrO(squareNumber) {
         //this condition checks whos turn it is.
         if (activePlayer === 'X') {
             //if activePlayer is equa to '0' the o.png is placed in HTML 
-            select.style.backgroundImage = 'url("images/o.png")';
+            select.style.backgroundImage = 'url("images/x.png")';
 
         } else {
             //if activePlayer is equal to '0', the o/png is placed in HTML
@@ -27,7 +27,7 @@ function placeXOrO(squareNumber) {
         //This condition is for changing the active player.
         if (activePlayer === 'X') {
             //if active player is 'X change to 'O'.
-            activePlayer = 'o';
+            activePlayer = 'O';
             //if active player is anything other than 'X'
         } else {
             //change the activePlayer to 'X'
@@ -35,7 +35,7 @@ function placeXOrO(squareNumber) {
         }
 
         //this function plays placement sound.
-        audio('SoundBible_files');
+        audio('media/winGame.mp3');
         //this condition checks to see if it is the computers turn.
         if (activePlayer === 'O') {
             //this function disables clicking for computers turn.
@@ -56,7 +56,7 @@ function computersTurn() {
     //this conditon allows our while loop to keep trying if a square is selected already.
     while (!success) {
         //a random number between 0 and 8 is selected.
-        pickASquare = string(Math.floor(Math.random() * 9));
+        pickASquare = String(Math.floor(Math.random() * 9));
         //if the random number evaluated returns true, the square hasn't been selected yet.
         if (placeXOrO(pickASquare)) {
             //this line calls the function.
@@ -74,15 +74,22 @@ function checkWinConditions() {
     // X 3, 4, 5 condition.
     else if (arrayIncludes('3X', '4X', '5X')) {drawWinLine(50, 304, 558, 304) }
     // X 6, 7, 8 condition.
-    else if (arrayIncludes('6X', '7X', '8X')) {drawWinLine(100, 50, 100, 558) }
-    else if (arrayIncludes('0X', '3X', '6X')) {drawWinLine(304, 50, 304, 558) }
-    else if (arrayIncludes('00', '10', '20')) {drawWinLine(50, 100, 558, 100) }
+    else if (arrayIncludes('6X', '7X', '8X')) {drawWinLine(50, 508, 558, 508) }
+    else if (arrayIncludes('0X', '3X', '6X')) {drawWinLine(100, 50, 100, 558) }
+    else if (arrayIncludes('1X', '4X', '7X')) {drawWinLine(304, 50, 304, 558) }
+    else if (arrayIncludes('2X', '5X', '8X')) {drawWinLine(508, 50, 508, 558) }
+    else if (arrayIncludes('6X', '4X', '2X')) {drawWinLine(100, 508, 510, 90) }
+    else if (arrayIncludes('0X', '4X', '8X')) {drawWinLine(100, 100, 520, 520) }
+    else if (arrayIncludes('0O', '10', '20')) {drawWinLine(50, 100, 558, 100) }
+    else if (arrayIncludes('30', '40', '50')) {drawWinLine(50, 304, 558,304) }
+    else if (arrayIncludes('60', '70', '80')) {drawWinLine(50, 508, 558, 508) }
+    else if (arrayIncludes('0O', '30', '60')) {drawWinLine(100, 50, 100, 558) }
     else if (arrayIncludes('10', '40', '70')) {drawWinLine(304, 50, 304, 558) }
-    else if (arrayIncludes('20', '50', '80')) {drawWinLine(508, 50, 508, 559) }
+    else if (arrayIncludes('20', '50', '80')) {drawWinLine(508, 50, 508,558) }
     else if (arrayIncludes('60', '40', '20')) {drawWinLine(100, 508, 510, 90) }
-    else if (arrayIncludes('0O', '40', '80')) {drawWinLine(100, 100, 520, 520) }
+    else if (arrayIncludes('0O', '40', '80')) {drawWinLine(100, 100, 520, 520)}
     else if (selectedSquares.length >= 9) {
-        audio('SoundBible_files');
+        audio('./media/winGame.mp3');
         setTimeout(function () { resetGame();}, 500);
     }
 }
@@ -97,9 +104,9 @@ function arrayIncludes(squareA, squareB, squareC) {
 //this function makes our body element temorarily unclickable.
 function disableClick() {
     //this makes our body unclickable.
-    ReportBody.style.pointerEvents = 'none';
+    body.style.pointerEvents = 'none';
     //this makes our body clickable again after 1 second.
-    setTimeout(function () { ReportBody.style.pointerEvents = 'auto'; }, 1000);
+    setTimeout(function () { body.style.pointerEvents = 'auto'; }, 1000);
 }
 
 //this function takes a string parameter of the path you set earlier for
@@ -120,10 +127,12 @@ function drawWinLine(coordX1, coordY1, coordX2, coordY2) {
     //this line indicates where the start of a lines x axis is.
     let x1 = coordX1,
     //this line indicates where the start of a lines y axis is.
-    y1 = coorX2,
+    y1 = coordY1,
     //this line indicates where the end of the a lines y axis is.
-    y2 = coordY2,
+    x2 = coordX2,
     //this variable stores temporary x axis data we update in our animation loop.
+    y2 = coordY2,
+    //this variable stores temorary x asis data we update in our animation loop
     x = x1,
     //this variable stores temporary y axis data we update in our animation loop.
     y = y1;
@@ -134,11 +143,11 @@ function drawWinLine(coordX1, coordY1, coordX2, coordY2) {
         //this variable creates a loop.
         const animationLoop = requestAnimationFrame(animateLineDrawing);
         //this method starts a new path.
-        c.clearReact(0, 0, 608, 608);
+        c.clearRect(0, 0, 608, 608);
         //this method starts a new path.
         c.beginPath();
         //this method moves us to a starting point in our line.
-        c. moveT0(x1, y1);
+        c. moveTo(x1, y1);
         //this method indicates the end point in our line.
         c.lineTo(x, y);
         //this method sets the width of our line.
@@ -148,7 +157,7 @@ function drawWinLine(coordX1, coordY1, coordX2, coordY2) {
         //this method draw everything we laid out above.
         c.stroke();
         //this condition checks if we've reached the endpoints.
-        if (X1 <= x2 && y1 <= y2) {
+        if (x1 <= x2 && y1 <= y2) {
             //this condition adds 10 to previous end x endpoint.
             if (x < x2) { x += 10; }
             //this condition adds 10 to the previous end y endpoint.
@@ -181,7 +190,7 @@ function clear() {
 //this line disallows clicking while the win sound is playing
 disableClick();
 //this line plays the win sounds.
-audio('js/SoundBible.com_files');
+audio('media/winGame.mp3');
 //this line calls our main animation loop.
 animateLineDrawing();
 //this line waits 1 second. Then, clears canvas, resets game, an alows clicking again.
